d94512b900051e7ce1ee854910ae12570dc75ce6
绚丽彩虹 <flandrestudio.cn@gmail.com>
2021-10-30 18:01:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/ccding/go-stun/stun"
 )
@@ -11,6 +12,7 @@ import (
 func main() {
 	var serverAddr = flag.String("s", "stun.qwq.pink:3478", "STUN server address")
 	var debugMode = flag.Bool("debug", false, "debug mode")
+	var returnJson = flag.Bool("json", false, "return json")
 
 	flag.Parse()
 
@@ -23,12 +25,38 @@ func main() {
 
 	nat, host, err := client.Discover()
 	if err != nil {
-		fmt.Print(`{"code": -1, "msg": "` + err.Error() + `"}`)
+		if *returnJson {
+			fmt.Print(`{"code": -1, "msg": "` + err.Error() + `"}`)
+		} else {
+			fmt.Print(strings.Join([]string{
+				"-1",
+				err.Error(),
+			}, "|"))
+		}
 		return
 	}
 	if host != nil {
-		fmt.Print(`{"code": 1, "msg": "OK", "nat": "` + nat.String() + `", "ip": "` + host.IP() + `", "port": "` + strconv.Itoa(int(host.Port())) + `", "ip_family": "` + strconv.Itoa(int(host.Family())) + `"}`)
-	}else{
-		fmt.Print(`{"code": 0, "msg": "NO_HOST", "nat": "` + nat.String() + `"}`)
+		if *returnJson {
+			fmt.Print(`{"code": 1, "msg": "OK", "nat": "` + nat.String() + `", "ip": "` + host.IP() + `", "port": "` + strconv.Itoa(int(host.Port())) + `", "ip_family": "` + strconv.Itoa(int(host.Family())) + `"}`)
+		} else {
+			fmt.Print(strings.Join([]string{
+				"1",
+				"OK",
+				strings.Replace(nat.String(), " ", "-", -1),
+				host.IP(),
+				strconv.Itoa(int(host.Port())),
+				strconv.Itoa(int(host.Family())),
+			}, "|"))
+		}
+	} else {
+		if *returnJson {
+			fmt.Print(`{"code": 0, "msg": "NO_HOST", "nat": "` + nat.String() + `"}`)
+		} else {
+			fmt.Print(strings.Join([]string{
+				"0",
+				"NO_HOST",
+				strings.Replace(nat.String(), " ", "-", -1),
+			}, "|"))
+		}
 	}
 }
